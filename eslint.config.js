5800dd49df6d11d70911c9c8b14c5db6b243fc29
messias1976 <messiasmachado1976@gmail.com>
2026-03-05 09:05:54 -0300

    chore: setup ESLint flat config with prettier and import sorting

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  { plugins: { 
+    "simpleImportSort": simpleImportSort,
+   }, 
+   rules: { 
+    "simple-import-sort/imports": "error", 
+    "simple-import-sort/exports": "error" 
+    },
+  },
+]);
